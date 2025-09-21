026aa2be34f07e88c168c83e2b49a2ac0dc20098
Subhashree <subhabalasubramanian2@gmail.com>
2025-09-21 14:35:41 +0530

    Add Place Order feature with DB transaction and analytics update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { updateTrending, getTopTrending, updateCoPurchase, getRecommendations } = require('./services/analyticsService');
 const Category = require('../models/Category');
 const { calculateCategorySales } = require('./services/categoryService');
+const { placeOrder } = require('./services/orderService');
 
 updateTrending('Laptop', 3);
 updateTrending('Mouse', 5);
@@ -17,3 +18,12 @@ laptopCategory.products = [{id:1}, {id:2}];
 const productsMap = new Map([[1, 50000], [2, 1500]]);
 
 console.log("Total Revenue for Laptops:", calculateCategorySales(laptopCategory, productsMap));
+
+const orderItems = [
+    {productId:1, productName:'Laptop', quantity:1},
+    {productId:2, productName:'Mouse', quantity:1}
+];
+
+placeOrder(1, orderItems, (orderId) => {
+    console.log("Order placed:", orderId);
+});
