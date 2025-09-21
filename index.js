79290f20103b1d59afed2a8fa3a86d733a5c2714
Subhashree <subhabalasubramanian2@gmail.com>
2025-09-21 14:31:00 +0530

    Add Recommendations feature using co-purchase Map

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const { updateTrending, getTopTrending } = require('./services/analyticsService');
+const { updateTrending, getTopTrending, updateCoPurchase, getRecommendations } = require('./services/analyticsService');
 
 updateTrending('Laptop', 3);
 updateTrending('Mouse', 5);
 
 console.log("Top Trending Products:", getTopTrending());
+
+updateCoPurchase(['Laptop','Mouse','Keyboard']);
+
+console.log("Recommendations for Laptop:", getRecommendations('Laptop'));
